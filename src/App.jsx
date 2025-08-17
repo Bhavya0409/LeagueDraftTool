d07ca57904fdec56a4c647c7f60d6628d9d2497b
Bhavya Shah <bhavya0409@gmail.com>
2025-08-16 23:21:37 -0400

    Add all champion images to assets/champs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import fs from 'fs'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [champs, setChamps] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  console.log(fs)
+
+  return ''
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch('https://ddragon.leagueoflegends.com/cdn/15.15.1/data/en_US/champion.json').then(res => res.json())
+  //     setChamps(Object.keys(res.data))
+  //   }
+  //   fetchData()
+  // }, [])
+
+  // useEffect(() => {
+  //   const fetchImage = async () => {
+  //     const res = await fetch('https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/Yunara.png')
+  //     console.log({res})
+  //   }
+  //   // fetchImage()
+  // })
+
+  // return (
+  //   <>
+  //     {champs.map(champ => <img src={`https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/${champ}.png`}/>)}
+  //   </>
+      
+  // )
 }
 
 export default App
---
+++ src/getChampImages.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Initial setup
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const outputDir = path.join(__dirname, '../src/assets/champs');
+fs.mkdirSync(outputDir, { recursive: true });
+
+/**
+ * Fetches an image from a URL and saves it to a specified path.
+ * @returns {Promise<void>}
+ */
+const fetchImage = async (champName) => {
+    const imageUrl = `https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/${champName}.png`;
+    const outputPath = path.join(outputDir, `${champName}.png`);
+
+    const res = await fetch(imageUrl)
+    const blob = await res.blob()
+
+    const buffer = await blob.arrayBuffer()
+    const bufferFrom = Buffer.from(buffer)
+
+    fs.createWriteStream(outputPath).write(bufferFrom)
+}
+const fetchImages = async () => {
+    const champsUrl = 'https://ddragon.leagueoflegends.com/cdn/15.15.1/data/en_US/champion.json';
+    const response = await fetch(champsUrl);
+    const data = await response.json();
+    const champNames = Object.keys(data.data);
+    return Promise.all(champNames.map(champName => fetchImage(champName)))
+}
+
+fetchImages()
+    .then(() => console.log('All images downloaded successfully'))
+    .catch(err => console.error('Error downloading images:', err));
