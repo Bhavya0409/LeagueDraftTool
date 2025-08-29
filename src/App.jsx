85eed01dbbc27804396751be3f86cd8409a15715
Bhavya Shah <bhavya0409@gmail.com>
2025-08-28 21:47:40 -0400

    Created skeleton; Added styled components; Added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,105 @@
-import { useState, useEffect } from 'react'
-import fs from 'fs'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import fs from "fs";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [champs, setChamps] = useState([])
+import styled from "styled-components";
 
-  console.log(fs)
+const Container = styled.div`
+  width: 1280px;
+  min-height: 750px;
+  display: grid;
+  grid-template-rows: auto auto 1fr;
+`;
+const SidesContainer = styled.div`
+  height: 48px;
+  background: #1e2a3aff;
+  display: flex;
+  flex-direction: row;
+`;
+const SideContainer = styled.div`
+  width: 50%;
+  height: 100%;
+`;
+const BlueSideContainer = styled(SideContainer)`
+  background: #1e2a3aff;
+`;
+const RedSideContainer = styled(SideContainer)`
+  background: #3e1e2aff;
+`;
+const BansContainer = styled.div`
+  height: 48px;
+  background: #1e2a3aff;
+  display: flex;
+  flex-direction: row;
+`;
+const BanContainer = styled.div`
+  width: 50%;
+  height: 100%;
+  background: #3a1e35ff;
+`;
+const BlueSideBanContainer = styled(BanContainer)``;
+const RedSideBanContainer = styled(BanContainer)``;
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 8fr 1fr;
+  height: 100%;
+`;
+const ColumnContainer = styled.div`
+  height: 100%;
+  display: grid;
+  row-gap: 8px;
+  grid-template-rows: auto auto auto 1fr auto auto;
+`;
+const SideColumnContainer = styled(ColumnContainer)`
+  margin: auto;
+`;
+const LeftColumnContainer = styled(SideColumnContainer)``;
+const MiddleColumnContainer = styled(ColumnContainer)`
+  background: #775130ff;
+`;
+const RightColumnContainer = styled(SideColumnContainer)``;
+const ChampionContainer = styled.div`
+  background: black;
+  width: 112px;
+  height: 112px;
+  border-radius: 8px;
+`;
 
-  return ''
+const App = () => {
+  return (
+    <Container>
+      <SidesContainer>
+        <BlueSideContainer>Blue</BlueSideContainer>
+        <RedSideContainer>Red</RedSideContainer>
+      </SidesContainer>
+      <BansContainer>
+        <BlueSideBanContainer>Blue side bans</BlueSideBanContainer>
+        <RedSideBanContainer>Red side bans</RedSideBanContainer>
+      </BansContainer>
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch('https://ddragon.leagueoflegends.com/cdn/15.15.1/data/en_US/champion.json').then(res => res.json())
-  //     setChamps(Object.keys(res.data))
-  //   }
-  //   fetchData()
-  // }, [])
+      <MainContainer>
+        <LeftColumnContainer>
+          <ChampionContainer />
+          <ChampionContainer />
+          <ChampionContainer />
+          <div />
+          <ChampionContainer />
+          <ChampionContainer />
+        </LeftColumnContainer>
+        <MiddleColumnContainer>Middle Container</MiddleColumnContainer>
+        <RightColumnContainer>
+          <ChampionContainer />
+          <ChampionContainer />
+          <ChampionContainer />
+          <div />
+          <ChampionContainer />
+          <ChampionContainer />
+        </RightColumnContainer>
+      </MainContainer>
+    </Container>
+  );
+};
 
-  // useEffect(() => {
-  //   const fetchImage = async () => {
-  //     const res = await fetch('https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/Yunara.png')
-  //     console.log({res})
-  //   }
-  //   // fetchImage()
-  // })
-
-  // return (
-  //   <>
-  //     {champs.map(champ => <img src={`https://ddragon.leagueoflegends.com/cdn/15.15.1/img/champion/${champ}.png`}/>)}
-  //   </>
-      
-  // )
-}
-
-export default App
+export default App;
